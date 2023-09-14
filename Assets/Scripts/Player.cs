5b94ac06c65384183a5dcde42967f3dddbb0449d
Boiko739 <boiko.ie739@gmail.com>
2023-09-14 23:13:07 +0300

    Refactored the Input system
    And added arrow keys to manipulate the player
    Everything is done with an Input package I installed from the Package manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,32 +7,13 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 7f,
                   rotateSpeed = 10f;
+    [SerializeField]
+    private GameInput gameInput;
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
         transform.position += moveSpeed * Time.deltaTime * moveDir;
 
